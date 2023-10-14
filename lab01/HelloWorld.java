bb203c79af93029cc71ce5cf47baa11edd5f3b1e
Sumo <dothuyduong25022003@gmail.com>
2023-10-14 13:27:08 +0700

    helloWorld

---
+++ lab01/HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+   author : Do Thuy Duong 20215019
+   ClassID: 732872 
+ */
+
+public class HelloWorld {
+	
+    public static void main(String[] args) {
+        System.out.println("Xin chao moi nguoi!");
+        System.out.println("Bye Bye moi nguoi !");
+    }
+}
