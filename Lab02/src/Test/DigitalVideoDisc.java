a0c476493f39433401e60f73556cd0c4967c5966
Sumo <dothuyduong25022003@gmail.com>
2023-11-12 10:03:10 +0700

    push topic/passing parameter

--- Lab02/src/Test/DigitalVideoDisc.java
+++ Lab02/src/Test/DigitalVideoDisc.java
@@ -3,6 +3,8 @@ package Test;
 // Do Thuy Duong 20215019
 
 public class DigitalVideoDisc {
+	
+
 	private String title;
 	private String category;
 	private String director;
@@ -13,17 +15,33 @@ public class DigitalVideoDisc {
 	public String getTitle() {
 		return title;
 	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
 	public String getCategory() {
 		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
 	}
 	public String getDirector() {
 		return director;
 	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
 	public int getLenght() {
 		return lenght;
 	}
+	public void setLenght(int lenght) {
+		this.lenght = lenght;
+	}
 	public float getCost() {
 		return cost;
+	}
+	public void setCost(float cost) {
+		this.cost = cost;
 	}
 	
 	// Do Thuy Duong 20215019
